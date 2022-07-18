4c8e33d9f24340ec11de655ec188ca352988bc9b
Jonathan <jonathancqaa@gmail.com>
2022-07-18 10:12:49 -0300

    Exercicio Lista 4: Tabuada da Mallu

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" === TABUADA DA MALLU ===");
+
+            Console.Write("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            int calc, i = 0;
+
+            for (i = 0; i <= 10; i++) {
+                
+                calc = num * i;
+                Console.WriteLine($"{num} x {i} = {calc}");
+            }
+        }
+    }
+}
